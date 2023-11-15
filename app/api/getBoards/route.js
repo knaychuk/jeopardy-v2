a1270a03673d01f4d803e95a58e1d167250770e8
Kate <knaychuk@gmail.com>
2023-11-15 04:50:16 -0800

    fix discrepancies

--- app/api/getBoards/route.js
+++ app/api/getBoards/route.js
@@ -1,5 +1,5 @@
-import Board from '@/models/board.js'
-import { connectToDB } from '@/utils/database.js'
+import Board from '@/models/board'
+import { connectToDB } from '@/utils/database'
 
 export const GET = async (req) => {
   try {
