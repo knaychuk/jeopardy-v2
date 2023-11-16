e96053a5b26cf26cba7e62ca7265c97f29854c6f
Kate <knaychuk@gmail.com>
2023-11-15 18:39:09 -0800

    test fetch from client

--- app/page.jsx
+++ app/page.jsx
@@ -3,31 +3,31 @@
 import { useState, useEffect } from 'react'
 
 export default function Home() {
-  // const [boards, setBoards] = useState(null)
+  const [boards, setBoards] = useState(null)
 
-  // useEffect(() => {
-  //   const fetchBoards = async () => {
-  //     const response = await fetch('/api/getBoards')
-  //     const data = await response.json()
+  useEffect(() => {
+    const fetchBoards = async () => {
+      const response = await fetch('/api/getBoards')
+      const data = await response.json()
 
-  //     setBoards(data)
-  //   }
+      setBoards(data)
+    }
 
-  //   fetchBoards()
+    fetchBoards()
 
-  // }, [])
+  }, [])
 
   return (
    <div>
-    {/* {boards && boards.map((board) => (
-      <div>
+    {boards && boards.map((board) => (
+      <div key={board._id}>
         {board._id}
         {board.title}
         {board.password}
       </div>
-    ))} */}
+    ))}
 
-    Hello World!
+
    </div>
   )
 }
